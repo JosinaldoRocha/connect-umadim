0a9f3a8fa42ac4732a63f9a4e5d3a0e25357edc8
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2025-01-24 20:09:58 -0300

    feat: added the ProviderScope widget

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,20 +1,23 @@
 import 'package:connect_umadim_app/app_routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Conecta umadim',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ProviderScope(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Conecta umadim',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        initialRoute: AppRoutes().initialRoute,
+        routes: AppRoutes().allAppRoutes,
       ),
-      initialRoute: AppRoutes().initialRoute,
-      routes: AppRoutes().allAppRoutes,
     );
   }
 }
