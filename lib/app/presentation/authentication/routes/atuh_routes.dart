41d399b085a788dd569210136f084efa63549c8f
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2025-01-25 14:03:42 -0300

    feat: i created the '/register' route to SignUpPage screen

--- lib/app/presentation/authentication/routes/atuh_routes.dart
+++ lib/app/presentation/authentication/routes/atuh_routes.dart
@@ -1,7 +1,8 @@
+import 'package:connect_umadim_app/app/presentation/authentication/views/pages/sign_up/sign_up_page.dart';
 import 'package:flutter/material.dart';
 import '../../../core/navigator/navigator.dart';
 import '../../splash/views/pages/splash_page.dart';
-import '../views/pages/sign_in_page.dart';
+import '../views/pages/sign_in/sign_in_page.dart';
 
 class AuthRoutes extends IModuleRoutes {
   @override
@@ -11,5 +12,6 @@ class AuthRoutes extends IModuleRoutes {
   Map<String, Widget Function(BuildContext context)> get routes => {
         initialRoute: (_) => const SplashPage(),
         '/login': (context) => SignInPage(),
+        '/register': (context) => SignUpPage(),
       };
 }
