e32e9aec90782832473b37a8b77d570ab906911c
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2025-01-24 21:59:37 -0300

    fix: i changed the routes

--- lib/app_routes.dart
+++ lib/app_routes.dart
@@ -1,5 +1,4 @@
 import 'package:connect_umadim_app/app/presentation/authentication/routes/atuh_routes.dart';
-import 'package:connect_umadim_app/app/presentation/splash/views/pages/splash_page.dart';
 import 'package:flutter/material.dart';
 
 import 'app/core/navigator/navigator.dart';
@@ -11,10 +10,8 @@ class AppRoutes extends IAppRoutes {
       ];
 
   @override
-  String get initialRoute => '/auth/login';
+  String get initialRoute => '/auth';
 
   @override
-  Map<String, Widget Function(BuildContext p1)> get routes => {
-        '/': (_) => const SplashPage(),
-      };
+  Map<String, Widget Function(BuildContext p1)> get routes => {};
 }
