1a0adfe63025aa28770d106e29d20c40c77f26a5
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2025-01-25 14:03:35 -0300

    feat: added the bodyLarge and bodyMedium properties

--- lib/app/core/style/app_text.dart
+++ lib/app/core/style/app_text.dart
@@ -7,8 +7,8 @@ class AppText {
       titleLarge: GoogleFonts.poppins(fontSize: 28),
       titleMedium: GoogleFonts.poppins(fontSize: 24),
       titleSmall: GoogleFonts.poppins(fontSize: 16),
-      bodyLarge: GoogleFonts.roboto(),
-      bodyMedium: GoogleFonts.roboto(),
+      bodyLarge: GoogleFonts.roboto(fontSize: 18),
+      bodyMedium: GoogleFonts.roboto(fontSize: 16),
       bodySmall: GoogleFonts.roboto(fontSize: 12),
     );
   }
