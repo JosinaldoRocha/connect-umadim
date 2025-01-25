a3b868337baec5c00482ce663f5ee47812e097b8
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2025-01-24 21:54:17 -0300

    feat: I implemented the features to handle errors

---
+++ lib/app/core/helpers/errors/common_errors.dart
@@ -0,0 +1,12 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'common_errors.freezed.dart';
+
+@freezed
+class CommonError with _$CommonError {
+  const factory CommonError.unauthenticated() = CommonErrorUnauthenticated;
+  const factory CommonError.connection() = CommonErrorConnection;
+  const factory CommonError.badRequest() = CommonErrorBadRequest;
+  const factory CommonError.notFound() = CommonErrorNotFound;
+  const factory CommonError.undefined() = CommonErrorUndefined;
+}
---
+++ lib/app/core/helpers/errors/generate_errors.dart
@@ -0,0 +1,22 @@
+import 'common_errors.dart';
+
+class GenerateError {
+  static CommonError fromException(Exception error) {
+    // if (error is DioException) {
+    //   switch (error.response?.statusCode) {
+    //     case 400:
+    //       return const CommonError.badRequest();
+    //     case 404:
+    //       return const CommonError.notFound();
+    //     case 401:
+    //       return const CommonError.unauthenticated();
+    //     default:
+    //       return const CommonError.undefined();
+    //   }
+    // } else if (error is SocketException) {
+    //   return const CommonError.connection();
+    // } else {
+    return const CommonError.undefined();
+    //  }
+  }
+}
