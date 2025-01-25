505606b1224696c5b03e1ae532d54787d8e61f3b
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2025-01-25 13:48:21 -0300

    feat: i created the constants.dart file

---
+++ lib/app/core/constants/constants.dart
@@ -0,0 +1,42 @@
+import 'package:dropdown_textfield/dropdown_textfield.dart';
+
+import '../../data/enums/funciton_type_enum.dart';
+
+final functionTypeList = [
+  DropDownValueModel(
+    name: FunctionType.leader.text,
+    value: FunctionType.leader,
+  ),
+  DropDownValueModel(
+    name: FunctionType.regent.text,
+    value: FunctionType.regent,
+  ),
+  DropDownValueModel(
+    name: FunctionType.media.text,
+    value: FunctionType.media,
+  ),
+  DropDownValueModel(
+    name: FunctionType.receptionist.text,
+    value: FunctionType.receptionist,
+  ),
+  DropDownValueModel(
+    name: FunctionType.secretary.text,
+    value: FunctionType.secretary,
+  ),
+  DropDownValueModel(
+    name: FunctionType.concierge.text,
+    value: FunctionType.concierge,
+  ),
+  DropDownValueModel(
+    name: FunctionType.evangelism.text,
+    value: FunctionType.evangelism,
+  ),
+  DropDownValueModel(
+    name: FunctionType.events.text,
+    value: FunctionType.events,
+  ),
+  DropDownValueModel(
+    name: FunctionType.member.text,
+    value: FunctionType.member,
+  ),
+];
