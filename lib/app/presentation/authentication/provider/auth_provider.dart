f45f614d21c6ef65c4644f37be564b777b00cd0e
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2025-01-28 00:33:15 -0300

    feat: i created the signUpProvider and signInProvider providers

--- lib/app/presentation/authentication/provider/auth_provider.dart
+++ lib/app/presentation/authentication/provider/auth_provider.dart
@@ -2,10 +2,24 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../data/providers/data_provider.dart';
 import '../state/authentication/check_authentication_state_notifier.dart';
+import '../state/sign_in/sign_in_state_notifier.dart';
+import '../state/sign_up/sign_up_state_notifier.dart';
 
 final authenticationState =
     StateNotifierProvider<CheckAutenticationStatenotifier, Autenticationstate>(
   (ref) => CheckAutenticationStatenotifier(
+    dataSource: ref.read(authDataSourceProvider),
+  ),
+);
+
+final signUpProvider = StateNotifierProvider<SignUpStateNotifier, SignUpState>(
+  (ref) => SignUpStateNotifier(
+    dataSource: ref.read(authDataSourceProvider),
+  ),
+);
+
+final signInProvider = StateNotifierProvider<SignInStateNotifier, SignInState>(
+  (ref) => SignInStateNotifier(
     dataSource: ref.read(authDataSourceProvider),
   ),
 );
