651496648192c63ff820f0b8a3172361d4ab03fb
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2025-01-28 16:59:42 -0300

    fix: i changed the type of the SingUpState state and i created the errorMessage function

--- lib/app/presentation/authentication/state/sign_up/sign_up_state_notifier.dart
+++ lib/app/presentation/authentication/state/sign_up/sign_up_state_notifier.dart
@@ -1,9 +1,10 @@
+import 'package:connect_umadim_app/app/core/helpers/errors/sign_up_errors.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../../core/helpers/helpers.dart';
 import '../../../../data/data_source/auth_data_source.dart';
 import '../../../../data/models/user_model.dart';
 
-typedef SignUpState = CommonState<CommonError, bool>;
+typedef SignUpState = CommonState<String, bool>;
 
 class SignUpStateNotifier extends StateNotifier<SignUpState> {
   SignUpStateNotifier({required this.dataSource})
@@ -18,8 +19,18 @@ class SignUpStateNotifier extends StateNotifier<SignUpState> {
 
     final result = await dataSource.signUp(user: user);
     result.fold(
-      (l) => state = SignUpState.loadFailure(l),
+      (l) => state = SignUpState.loadFailure(errorMessage(l)),
       (r) => state = SignUpState.loadSuccess(r),
     );
+  }
+
+  String errorMessage(SignUpError error) {
+    if (error == SignUpError.emailAlreadyExists) {
+      return 'Este e-mail já está cadastrado. Tente outro ou recupere sua conta!';
+    } else if (error == SignUpError.noPermission) {
+      return 'Você não tem permissão para se cadastrar nesta função. Verifique com o administrador!';
+    } else {
+      return 'Ocorreu um erro inesperado. Por favor, tente novamente mais tarde!';
+    }
   }
 }
