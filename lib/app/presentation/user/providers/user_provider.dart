6bca717747e2fc4e6211a9bf14af01deb1e96219
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2025-01-28 23:30:27 -0300

    feat: i created the user_provider.dart file

---
+++ lib/app/presentation/user/providers/user_provider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../data/providers/data_provider.dart';
+import '../states/complete_profile/complete_profile_state_notifier.dart';
+import '../states/get_local_user/get_user_state_notifier.dart';
+
+final completeProfileProvider =
+    StateNotifierProvider<CompleteProfileStateNotifier, CompleteProfileState>(
+  (ref) => CompleteProfileStateNotifier(
+    dataSource: ref.read(userDataSourceProvider),
+  ),
+);
+
+final getUserProvider =
+    StateNotifierProvider<GetUserStateNotifier, GetUserState>(
+  (ref) => GetUserStateNotifier(
+    dataSource: ref.read(userDataSourceProvider),
+  ),
+);
