3b158ec00243c5999d40b3ea242e69b4cf0b79e7
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2025-01-28 23:27:08 -0300

    feat: i created the congregationsList and genderList constants

--- lib/app/core/constants/constants.dart
+++ lib/app/core/constants/constants.dart
@@ -1,3 +1,4 @@
+import 'package:connect_umadim_app/app/data/enums/congregation_enum.dart';
 import 'package:dropdown_textfield/dropdown_textfield.dart';
 
 import '../../data/enums/funciton_type_enum.dart';
@@ -38,5 +39,67 @@ final functionTypeList = [
   DropDownValueModel(
     name: FunctionType.member.text,
     value: FunctionType.member,
+  ),
+];
+
+final congregationsList = [
+  DropDownValueModel(
+    name: Congregation.temploCentral.text,
+    value: Congregation.temploCentral,
+  ),
+  DropDownValueModel(
+    name: Congregation.monteSinai.text,
+    value: Congregation.monteSinai,
+  ),
+  DropDownValueModel(
+    name: Congregation.lirioDosVales.text,
+    value: Congregation.lirioDosVales,
+  ),
+  DropDownValueModel(
+    name: Congregation.rosaDeSaron.text,
+    value: Congregation.rosaDeSaron,
+  ),
+  DropDownValueModel(
+    name: Congregation.valeDeBencaos.text,
+    value: Congregation.valeDeBencaos,
+  ),
+  DropDownValueModel(
+    name: Congregation.juda.text,
+    value: Congregation.juda,
+  ),
+  DropDownValueModel(
+    name: Congregation.novaJerusalem.text,
+    value: Congregation.novaJerusalem,
+  ),
+  DropDownValueModel(
+    name: Congregation.altoRefugio.text,
+    value: Congregation.altoRefugio,
+  ),
+  DropDownValueModel(
+    name: Congregation.monteDasOliveiras.text,
+    value: Congregation.monteDasOliveiras,
+  ),
+  DropDownValueModel(
+    name: Congregation.ebenezer.text,
+    value: Congregation.ebenezer,
+  ),
+  DropDownValueModel(
+    name: Congregation.maranata.text,
+    value: Congregation.maranata,
+  ),
+  DropDownValueModel(
+    name: Congregation.monteMoria.text,
+    value: Congregation.monteMoria,
+  ),
+];
+
+final genderList = [
+  DropDownValueModel(
+    name: 'Masculino',
+    value: 'masculino',
+  ),
+  DropDownValueModel(
+    name: 'Feminino',
+    value: 'feminino',
   ),
 ];
