4357dbfba65fa1ab0452aca21781bb38e748e467
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2025-01-28 23:32:33 -0300

    feat: i created the '/complete-profile' and '/complete-profile-form' routes to CompleteProfilePage and CompleteProfileFormPage respectively

--- lib/app/presentation/authentication/routes/atuh_routes.dart
+++ lib/app/presentation/authentication/routes/atuh_routes.dart
@@ -1,9 +1,11 @@
+import 'package:connect_umadim_app/app/presentation/authentication/views/pages/complete_profile_form/complete_profile_form_page.dart';
 import 'package:connect_umadim_app/app/presentation/authentication/views/pages/email_sent/email_sent_page.dart';
 import 'package:connect_umadim_app/app/presentation/authentication/views/pages/recover_password/recover_password_page.dart';
 import 'package:connect_umadim_app/app/presentation/authentication/views/pages/sign_up/sign_up_page.dart';
 import 'package:flutter/material.dart';
 import '../../../core/navigator/navigator.dart';
 import '../../splash/views/pages/splash_page.dart';
+import '../views/pages/complete_profile/complete_profile_page.dart';
 import '../views/pages/sign_in/sign_in_page.dart';
 
 class AuthRoutes extends IModuleRoutes {
@@ -17,5 +19,7 @@ class AuthRoutes extends IModuleRoutes {
         '/register': (context) => SignUpPage(),
         '/recover-password': (context) => RecoverPasswordPage(),
         '/email-sent': (context) => EmailSentPage(),
+        '/complete-profile': (context) => CompleteProfilePage(),
+        '/complete-profile-form': (context) => CompleteProfileFormPage(),
       };
 }
