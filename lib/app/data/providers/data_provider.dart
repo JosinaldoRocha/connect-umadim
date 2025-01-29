1bfc17f65843a52d9338a758e9220bd2d37ae0d2
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2025-01-28 23:30:10 -0300

    feat: i created the userDataSourceProvider provider

--- lib/app/data/providers/data_provider.dart
+++ lib/app/data/providers/data_provider.dart
@@ -1,7 +1,12 @@
+import 'package:connect_umadim_app/app/data/data_source/user_data_source.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../data_source/auth_data_source.dart';
 
 final authDataSourceProvider = Provider(
   (ref) => AuthDataSource(),
+);
+
+final userDataSourceProvider = Provider(
+  (ref) => UserDataSource(),
 );
