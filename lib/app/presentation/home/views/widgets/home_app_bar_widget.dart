36dcf31fa1f67873d923a9da3417c1bf35f702f5
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2025-01-31 16:50:51 -0300

    feat: i created the HomeAppBarWidget widget

---
+++ lib/app/presentation/home/views/widgets/home_app_bar_widget.dart
@@ -0,0 +1,93 @@
+import 'package:connect_umadim_app/app/widgets/spacing/spacing.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../../core/style/app_text.dart';
+import '../../../../widgets/image/profile_image_widget.dart';
+import '../../../user/providers/user_provider.dart';
+import 'loading_home_app_bar_widget.dart';
+
+class HomeAppBarWidget extends ConsumerStatefulWidget {
+  const HomeAppBarWidget({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _HomeAppBarWidgetState();
+}
+
+class _HomeAppBarWidgetState extends ConsumerState<HomeAppBarWidget> {
+  @override
+  Widget build(BuildContext context) {
+    final userState = ref.watch(getUserProvider);
+
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 16,
+        right: 16,
+        top: 48,
+      ),
+      child: userState.maybeWhen(
+        loadInProgress: () => LoadingHomeAppBarWidget(),
+        loadSuccess: (data) => Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SpaceHorizontal.x1(),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'A paz do Senhor ✋',
+                  style: AppText.text().titleSmall,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      '${data.umadimFunction ?? (data.localFunction != 'Membro' ? data.localFunction : '')} ',
+                      style: AppText.text().titleSmall!.copyWith(),
+                    ),
+                    Text(
+                      getFirstAndSecondName(data.name),
+                      style: AppText.text().titleMedium!.copyWith(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                          ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Spacer(),
+            ProfileImageWidget(
+              image: data.photoUrl,
+              size: 52,
+            ),
+          ],
+        ),
+        orElse: () => Container(),
+      ),
+    );
+  }
+
+  String getGreetingMessage() {
+    final hour = DateTime.now().hour;
+
+    if (hour >= 5 && hour < 12) {
+      return 'Bom dia';
+    } else if (hour >= 12 && hour < 18) {
+      return 'Boa tarde';
+    } else {
+      return 'Boa noite';
+    }
+  }
+
+  String getFirstAndSecondName(String fullName) {
+    final names = fullName.split(' ');
+    if (names.length >= 2) {
+      return '${names[0]} ${names[1]}';
+    }
+    return names[0];
+  }
+}
