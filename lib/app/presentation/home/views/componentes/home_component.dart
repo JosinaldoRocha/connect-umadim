6536dc906bfb6e09ac76315acb451e73e27b569e
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2025-02-02 16:15:16 -0300

    feat: i called the ref.read method and added the Container and BirthdaysWeekWidget widgets

--- lib/app/presentation/home/views/componentes/home_component.dart
+++ lib/app/presentation/home/views/componentes/home_component.dart
@@ -1,7 +1,9 @@
+import 'package:connect_umadim_app/app/presentation/home/views/widgets/birthdays_week_widget.dart';
 import 'package:connect_umadim_app/app/presentation/home/views/widgets/home_app_bar_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../../core/style/app_colors.dart';
 import '../../../user/providers/user_provider.dart';
 
 class HomeComponent extends ConsumerStatefulWidget {
@@ -15,17 +17,23 @@ class _HomeComponentState extends ConsumerState<HomeComponent> {
   @override
   void initState() {
     super.initState();
-    Future.microtask(
-      () => ref.read(getUserProvider.notifier).load(),
-    );
+    Future.microtask(() {
+      ref.read(getUserProvider.notifier).load();
+      ref.read(listUsersProvider.notifier).load();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        HomeAppBarWidget(),
-      ],
+    return Container(
+      color: AppColor.lightBgColor,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          HomeAppBarWidget(),
+          BirthdaysWeekWidget(),
+        ],
+      ),
     );
   }
 }
