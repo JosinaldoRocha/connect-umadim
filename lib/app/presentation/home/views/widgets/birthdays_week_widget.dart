d48ecd0af21c05fd6314c9ba623c4ce30d3be2b2
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2025-02-02 16:12:11 -0300

    feat: i created tha BirthdaysWeekWidget widget

---
+++ lib/app/presentation/home/views/widgets/birthdays_week_widget.dart
@@ -0,0 +1,85 @@
+import 'package:connect_umadim_app/app/core/style/app_colors.dart';
+import 'package:connect_umadim_app/app/core/style/app_text.dart';
+import 'package:connect_umadim_app/app/presentation/home/views/widgets/birthday_item_widget.dart';
+import 'package:connect_umadim_app/app/presentation/user/providers/user_provider.dart';
+import 'package:connect_umadim_app/app/widgets/spacing/spacing.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../../data/models/user_model.dart';
+import 'birthdays_list_widget.dart';
+
+class BirthdaysWeekWidget extends ConsumerWidget {
+  const BirthdaysWeekWidget({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userListState = ref.watch(listUsersProvider);
+
+    return userListState.maybeWhen(
+      loadSuccess: (data) {
+        final birthdayUsers = filterUsersByBirthdayWeek(data);
+
+        return birthdayUsers.isEmpty
+            ? Container()
+            : Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SpaceVertical.x5(),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8),
+                    child: Text(
+                      birthdayUsers.length > 1
+                          ? 'Aniversariantes da semana 🎈'
+                          : 'Aniversariante da semana 🎈',
+                      style: AppText.text().titleSmall,
+                    ),
+                  ),
+                  Container(
+                    height: 244,
+                    color: AppColor.white,
+                    margin: EdgeInsets.symmetric(vertical: 4),
+                    child: birthdayUsers.length == 1
+                        ? Center(
+                            child: BirthdayItemWidget(user: birthdayUsers[0]),
+                          )
+                        : BirthdaysListWidget(birthdayUsers: birthdayUsers),
+                  ),
+                ],
+              );
+      },
+      orElse: () => Container(),
+    );
+  }
+
+  List<UserModel> filterUsersByBirthdayWeek(List<UserModel> users) {
+    final now = DateTime.now();
+    final startOfWeek = now.subtract(Duration(days: now.weekday % 7));
+    final endOfWeek = startOfWeek.add(const Duration(days: 6));
+
+    final filteredUsers = users.where((user) {
+      final birthDate = user.birthDate;
+      if (birthDate == null) return false;
+
+      final birthThisYear = DateTime(now.year, birthDate.month, birthDate.day);
+      return birthThisYear
+              .isAfter(startOfWeek.subtract(const Duration(days: 1))) &&
+          birthThisYear.isBefore(endOfWeek.add(const Duration(days: 1)));
+    }).toList();
+
+    filteredUsers.sort((a, b) {
+      int getWeekday(DateTime date) {
+        return date.weekday == 7 ? 0 : date.weekday;
+      }
+
+      final aDayOfWeek =
+          getWeekday(DateTime(now.year, a.birthDate!.month, a.birthDate!.day));
+      final bDayOfWeek =
+          getWeekday(DateTime(now.year, b.birthDate!.month, b.birthDate!.day));
+
+      return aDayOfWeek.compareTo(bDayOfWeek);
+    });
+
+    return filteredUsers;
+  }
+}
