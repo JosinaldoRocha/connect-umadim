615aa2126619b18167ae452b896e31f206efbce4
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2025-02-02 16:13:53 -0300

    feat: i created the listUserProvider provider

--- lib/app/presentation/user/providers/user_provider.dart
+++ lib/app/presentation/user/providers/user_provider.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../data/providers/data_provider.dart';
 import '../states/complete_profile/complete_profile_state_notifier.dart';
 import '../states/get_local_user/get_user_state_notifier.dart';
+import '../states/list_users/agents_list_state_notifier.dart';
 
 final completeProfileProvider =
     StateNotifierProvider<CompleteProfileStateNotifier, CompleteProfileState>(
@@ -14,6 +15,13 @@ final completeProfileProvider =
 final getUserProvider =
     StateNotifierProvider<GetUserStateNotifier, GetUserState>(
   (ref) => GetUserStateNotifier(
+    dataSource: ref.read(userDataSourceProvider),
+  ),
+);
+
+final listUsersProvider =
+    StateNotifierProvider<ListUsersStateNotifier, ListUsersState>(
+  (ref) => ListUsersStateNotifier(
     dataSource: ref.read(userDataSourceProvider),
   ),
 );
