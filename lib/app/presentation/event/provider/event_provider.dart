b9658f1c6a9573e8cb4a5efc5d201b2f316215a6
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2025-02-06 11:50:43 -0300

    feat: i created the event_provider.dart file

---
+++ lib/app/presentation/event/provider/event_provider.dart
@@ -0,0 +1,23 @@
+import 'package:connect_umadim_app/app/data/data_source/event_data_source.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../states/add/add_event_state_notifier.dart';
+import '../states/get/get_all_events_state_notifier.dart';
+
+final eventDataSourceProvider = Provider(
+  (ref) => EventDataSource(),
+);
+
+final getAllEventProvider =
+    StateNotifierProvider<GetAllEventsStateNotifier, GetAllEventsState>(
+  (ref) => GetAllEventsStateNotifier(
+    dataSource: ref.read(eventDataSourceProvider),
+  ),
+);
+
+final addEventProvider =
+    StateNotifierProvider<AddEventStateNotifier, AddEventState>(
+  (ref) => AddEventStateNotifier(
+    dataSource: ref.read(eventDataSourceProvider),
+  ),
+);
