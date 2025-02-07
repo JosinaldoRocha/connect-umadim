7851e2b91c12983b279f3881820b4c0a9190dafa
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2025-02-07 11:05:59 -0300

    feat: i created the NextEventWidget widget

---
+++ lib/app/presentation/event/views/widgets/next_event_widget.dart
@@ -0,0 +1,64 @@
+import 'package:connect_umadim_app/app/presentation/event/provider/event_provider.dart';
+import 'package:connect_umadim_app/app/presentation/event/views/widgets/event_item_widget.dart';
+import 'package:connect_umadim_app/app/presentation/event/views/widgets/next_event_list_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../../core/style/app_text.dart';
+
+class NextEventWidget extends ConsumerStatefulWidget {
+  const NextEventWidget({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _NextEventWidgetState();
+}
+
+class _NextEventWidgetState extends ConsumerState<NextEventWidget> {
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(
+      () => ref.read(getNextEventProvider.notifier).load(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final state = ref.watch(getNextEventProvider);
+
+    return state.maybeWhen(
+      loadSuccess: (data) => data.isEmpty
+          ? Container()
+          : Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: 16,
+                    top: 20,
+                    bottom: 4,
+                  ),
+                  child: Text(
+                    data.length > 1 ? 'Próximos eventos' : 'Próximo evento',
+                    style: AppText.text().titleSmall,
+                  ),
+                ),
+                data.length == 1
+                    ? Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        child: EventItemWidget(
+                          event: data[0],
+                          isHorizontalScrolling: false,
+                        ),
+                      )
+                    : SizedBox(
+                        height: 204,
+                        child: NextEventListWidget(events: data),
+                      ),
+              ],
+            ),
+      orElse: () => Container(),
+    );
+  }
+}
