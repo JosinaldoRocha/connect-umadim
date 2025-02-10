b251ff713e695344ba8c8581980b71a4cd632633
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2025-02-10 11:37:03 -0300

    feat: i called the color property

--- lib/app/core/style/app_text.dart
+++ lib/app/core/style/app_text.dart
@@ -1,15 +1,34 @@
+import 'package:connect_umadim_app/app/core/style/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppText {
   static TextTheme text() {
     return TextTheme(
-      titleLarge: GoogleFonts.poppins(fontSize: 28),
-      titleMedium: GoogleFonts.poppins(fontSize: 24),
-      titleSmall: GoogleFonts.poppins(fontSize: 16),
-      bodyLarge: GoogleFonts.roboto(fontSize: 18),
-      bodyMedium: GoogleFonts.roboto(fontSize: 16),
-      bodySmall: GoogleFonts.roboto(fontSize: 12),
+      titleLarge: GoogleFonts.poppins(
+        fontSize: 28,
+        color: AppColor.tertiary,
+      ),
+      titleMedium: GoogleFonts.poppins(
+        fontSize: 24,
+        color: AppColor.tertiary,
+      ),
+      titleSmall: GoogleFonts.poppins(
+        fontSize: 16,
+        color: AppColor.tertiary,
+      ),
+      bodyLarge: GoogleFonts.roboto(
+        fontSize: 18,
+        color: AppColor.tertiary,
+      ),
+      bodyMedium: GoogleFonts.roboto(
+        fontSize: 16,
+        color: AppColor.tertiary,
+      ),
+      bodySmall: GoogleFonts.roboto(
+        fontSize: 12,
+        color: AppColor.tertiary,
+      ),
     );
   }
 }
