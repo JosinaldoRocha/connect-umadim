006ba9a15c6e28b05f54356ff567e4897c5b351d
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2025-02-10 11:36:35 -0300

    feat: i created the them, singer, minister and confirmedPresences parameters and copyWith function

--- lib/app/data/models/event_model.dart
+++ lib/app/data/models/event_model.dart
@@ -1,4 +1,6 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'package:connect_umadim_app/app/data/enums/department_enum.dart';
 import 'package:connect_umadim_app/app/data/enums/event_status_enum.dart';
 import 'package:connect_umadim_app/app/data/enums/event_type_enum.dart';
@@ -15,7 +17,12 @@ class EventModel {
   DateTime eventTime;
   Department promotedBy;
   String? description;
+  String? theme;
+  String? singer;
+  String? minister;
+  List<String> confirmedPresences;
   DateTime createdAt;
+
   EventModel({
     required this.id,
     required this.userId,
@@ -28,6 +35,10 @@ class EventModel {
     required this.eventTime,
     required this.promotedBy,
     this.description,
+    this.theme,
+    this.singer,
+    this.minister,
+    required this.confirmedPresences,
     required this.createdAt,
   });
 
@@ -44,6 +55,10 @@ class EventModel {
       'eventTime': Timestamp.fromDate(eventTime),
       'promotedBy': promotedBy.text,
       'description': description,
+      'theme': theme,
+      'singer': singer,
+      'minister': minister,
+      'confirmedPresences': confirmedPresences,
       'createdAt': Timestamp.fromDate(createdAt),
     };
   }
@@ -70,7 +85,52 @@ class EventModel {
       description: snapshot['description'] != null
           ? snapshot['description'] as String
           : null,
+      theme: snapshot['theme'] != null ? snapshot['theme'] as String : null,
+      singer: snapshot['singer'] != null ? snapshot['singer'] as String : null,
+      minister:
+          snapshot['minister'] != null ? snapshot['minister'] as String : null,
+      confirmedPresences: (snapshot['confirmedPresences'] as List)
+          .map((e) => e.toString())
+          .toList(),
       createdAt: (snapshot['createdAt'] as Timestamp).toDate().toLocal(),
+    );
+  }
+
+  EventModel copyWith({
+    String? id,
+    String? userId,
+    String? title,
+    EventType? type,
+    EventStatus? status,
+    String? imageUrl,
+    String? eventLocation,
+    DateTime? eventDate,
+    DateTime? eventTime,
+    Department? promotedBy,
+    String? description,
+    String? theme,
+    String? singer,
+    String? minister,
+    List<String>? confirmedPresences,
+    DateTime? createdAt,
+  }) {
+    return EventModel(
+      id: id ?? this.id,
+      userId: userId ?? this.userId,
+      title: title ?? this.title,
+      type: type ?? this.type,
+      status: status ?? this.status,
+      imageUrl: imageUrl ?? this.imageUrl,
+      eventLocation: eventLocation ?? this.eventLocation,
+      eventDate: eventDate ?? this.eventDate,
+      eventTime: eventTime ?? this.eventTime,
+      promotedBy: promotedBy ?? this.promotedBy,
+      description: description ?? this.description,
+      theme: theme ?? this.theme,
+      singer: singer ?? this.singer,
+      minister: minister ?? this.minister,
+      confirmedPresences: confirmedPresences ?? this.confirmedPresences,
+      createdAt: createdAt ?? this.createdAt,
     );
   }
 }
