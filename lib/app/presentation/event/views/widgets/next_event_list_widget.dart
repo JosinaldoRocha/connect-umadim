d54e3781ca5356e1251e73863306864b05d7ccb8
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2025-02-12 10:08:09 -0300

    feat: i created the sortedEvents parameter

--- lib/app/presentation/event/views/widgets/next_event_list_widget.dart
+++ lib/app/presentation/event/views/widgets/next_event_list_widget.dart
@@ -1,3 +1,4 @@
+import 'package:connect_umadim_app/app/data/enums/department_enum.dart';
 import 'package:connect_umadim_app/app/data/models/event_model.dart';
 import 'package:connect_umadim_app/app/presentation/event/views/widgets/event_item_widget.dart';
 import 'package:flutter/material.dart';
@@ -14,6 +15,14 @@ class NextEventListWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final sortedEvents = [...events]..sort(
+        (a, b) {
+          if (a.promotedBy == Department.umadim) return -1;
+          if (b.promotedBy == Department.umadim) return 1;
+          return 0;
+        },
+      );
+
     return Center(
       child: ListView.separated(
         shrinkWrap: true,
@@ -21,15 +30,15 @@ class NextEventListWidget extends StatelessWidget {
         itemBuilder: (context, index) => Container(
           margin: EdgeInsets.only(
             left: index == 0 ? 16 : 0,
-            right: index == events.length - 1 ? 16 : 0,
+            right: index == sortedEvents.length - 1 ? 16 : 0,
           ),
           child: EventItemWidget(
-            event: events[index],
+            event: sortedEvents[index],
             isHorizontalScrolling: true,
           ),
         ),
         separatorBuilder: (context, index) => SpaceHorizontal.x2(),
-        itemCount: events.length,
+        itemCount: sortedEvents.length,
       ),
     );
   }
