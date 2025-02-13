fd79075e4eb2581bf9fa52f8ca7ee40bf03f62c6
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2025-02-13 16:54:23 -0300

    feat: i created the getAllVersesProvider provider

--- lib/app/presentation/home/provider/home_provider.dart
+++ lib/app/presentation/home/provider/home_provider.dart
@@ -1,7 +1,16 @@
+import 'package:connect_umadim_app/app/data/providers/data_provider.dart';
+import 'package:connect_umadim_app/app/presentation/home/states/verses_list_state_notifier.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../states/tabs_state_notifier.dart';
 
 final homeTabsProvider = StateNotifierProvider<TabsStateNotifier, int>(
   (ref) => TabsStateNotifier(),
+);
+
+final getAllVersesProvider =
+    StateNotifierProvider<GetAllVersesStateNotifier, GetAllVersesState>(
+  (ref) => GetAllVersesStateNotifier(
+    dataSource: ref.read(userDataSourceProvider),
+  ),
 );
